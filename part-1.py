af8befb2d6f3504ccb42b1fcd30ff00adef6cd45
Olya Mateshov <olyamateshov@gmail.com>
2025-05-05 12:22:25 -0400

    Recursion Problem Set Submissions

--- part-1.py
+++ part-1.py
@@ -4,17 +4,33 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n < 0:
+        raise ValueError("Input must be a non-negative integer.")
+    if n == 1 or n == 0:
+        return 1
+    return n * factorial(n - 1)
 
 
 # reverse
-
+def reverse(text):
+    if len(text) <= 0:
+        return ""
+    return text[len(text) - 1] + reverse(text[:len(text) - 1])
 
 
 # bunny
-
+def bunny(n):
+    if n == 0:
+        return 0
+    return 2 + bunny(n - 1)
 
 
 # is_nested_parens
-
+def is_nested_parens(parens):
+    if len(parens) == 0:
+        return True
+    elif not parens[0] == '(' or not parens[len(parens)-1] == ')':
+        return False
+    return is_nested_parens(parens[1:-1])
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,38 @@
 # the appropriate comment.
 
 # search
-
+def search(array, query):
+    if len(array) == 0:
+        return False
+    elif array[0] == query:
+        return True
+    return search(array[1:], query)
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) <= 1:
+        return True
+    elif text[0] != text[len(text)-1]:
+        return False
+  
+    return is_palindrome(text[1:-1])
 
 
 # digit_match
+def digit_match(num1, num2):
+    num1_str = str(num1)
+    num2_str = str(num2)
+    return helper_fun(num1_str, num2_str)
 
+
+def helper_fun(num1_str, num2_str):
+    match = 0
+    if not num1_str or not num2_str:
+        return 0
+    if num1_str[len(num1_str)-1] == num2_str[len(num2_str)-1]:
+        match += 1
+    param1 = num1_str[:len(num1_str)-1]
+    param2 = num2_str[:len(num2_str)-1]
+    return match + helper_fun(param1, param2)
 
